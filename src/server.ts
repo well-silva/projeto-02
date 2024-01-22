892fa9258e6359eb823756bc9a8a38f843f1e825
Wellington Silva <well.silvaa97@gmail.com>
2024-01-22 16:09:37 -0300

    chore: ajustes para producao

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,5 @@ import { env } from './env';
 
 app.listen({
 	port: env.PORT,
+	host: ('RENDER' in process.env) ? '0.0.0.0' : 'localhost',
 }).then(() => console.log(`Server is running on port ${env.PORT}`));
